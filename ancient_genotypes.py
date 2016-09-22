ec85a2efb014f534e5d318a43387e819777af33e
schraiber <jgschraiber@gmail.com>
2016-09-22 13:46:37 +0100

    removing depricated functions for simulation

--- ancient_genotypes.py
+++ ancient_genotypes.py
@@ -107,6 +107,48 @@ def ancient_sample_mix(num_modern=1000,anc_pop = 0, anc_time=200,mix_time=300,sp
 			#raw_input()
 	return np.array(freq), anc
 
+def ancient_sample_mix_multiple(num_modern=1000,anc_pop = 0, anc_num = 1, anc_time=200,mix_time=300,split_time=400,f=0.0,Ne0=10000,Ne1=10000,mu=1.25e-8,length=1000,num_rep=1000,coverage=False):
+	if mix_time > split_time:
+		print "mixture occurs more anciently than population split!"
+		return None
+	if f < 0 or f > 1:
+		print "Admixture fraction is not in [0,1]"
+		return None
+	samples = [msp.Sample(population=0,time=0)]*num_modern
+	samples.extend([msp.Sample(population=anc_pop,time=anc_time)]*(2*anc_num))
+	pop_config = [msp.PopulationConfiguration(initial_size=Ne0),msp.PopulationConfiguration(initial_size=Ne1)]
+	divergence = [msp.MassMigration(time=mix_time,source=0,destination=1,proportion = f),
+			msp.MassMigration(time=split_time,source=1,destination=0,proportion=1.0)]
+	sims = msp.simulate(samples=samples,Ne=Ne0,population_configurations=pop_config,demographic_events=divergence,mutation_rate=mu,length=length,num_replicates=num_rep)
+	freq = []
+	anc = []
+	for sim in sims:
+		for position, variant in sim.variants():
+			var_array = map(int,list(variant))
+			cur_freq = sum(var_array[:-(2*anc_num)])/float(num_modern)
+			if cur_freq == 0 or cur_freq == 1: continue
+			freq.append(cur_freq)
+			if not coverage:
+				cur_site = np.array([0,0,0])
+				cur_site[sum(var_array[-2:])]=1
+				if len(cur_site) > 3:
+					print var_array[-2:]
+					print cur_site
+					raw_input()
+				anc.append(cur_site)
+			else:
+				for i in range(anc_num):
+					num_reads = st.poisson.rvs(coverage)
+					if i == 0: cur_GT = var_array[-2:]
+					else: cur_GT = var_array[-(2*(i+1)):-(2*i)],
+					reads = np.random.choice(cur_GT,size=num_reads, replace=True)
+					derived_reads = sum(reads)
+					anc.append((num_reads-derived_reads,derived_reads))
+			#print var_array[-2:]
+			#print anc
+			#raw_input()
+	return np.array(freq), anc
+
 def get_het_prob(freq,anc):
 	anc_dict = {}
 	for i in range(len(freq)):
@@ -302,7 +344,10 @@ def compute_genotype_sampling_probs(sampling_prob, GTs):
 ##the first level corresponds to the freqs in freq
 ##within each frequency, there are arrays of reads that can be passed to compute_read_like
 def compute_GT_like(reads,freq,t1,t2):
-	n_diploid = len(reads[0][0])
+	if reads[0][0].ndim == 1:
+		n_diploid = 1
+	else:
+		n_diploid = len(reads[0][0])
 	n_haploid = 2*n_diploid
 	GTs = generate_genotypes(n_diploid)	
 	Ey = compute_Ey(freq,n_haploid,t1,t2)
@@ -310,14 +355,9 @@ def compute_GT_like(reads,freq,t1,t2):
 	per_site_like = []
 	for i in range(len(freq)):
 		GT_prob = compute_genotype_sampling_probs(sampling_prob[i,:],GTs)
-		print freq[i]
-		print GT_prob
 		for j in range(len(reads[i])):
 			read_like = compute_read_like(reads[i][j],GTs)
-			print reads[i][j]
-			print read_like
 			cur_prob = sum(read_like*GT_prob)
 			per_site_like.append(cur_prob)	
-	return per_site_like
+	return np.log(per_site_like)
 
-#TODO:For actual likelihood, should have a dictionary of genotype likelihoods corresponding to each frequency, so don't need to compute *too many* matrix Exponentials
